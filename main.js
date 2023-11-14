63ed070e3d691769dd1b87b7994efceab9ab7b12
Angelos Giannourakos <angelos.giannourakos@gmail.com>
2023-11-14 12:43:51 +0200

    adding values on buttons

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const screen = document.querySelector('#screen');
 let btns = document.querySelector('.items');
-
+let btn7 = document.querySelector('.btn-7');
 let result;
 
+btn7.addEventListener('click', () => {console.log(btn7.value)});
