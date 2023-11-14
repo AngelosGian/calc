4b0ad2dcfe0d7f43942e65cbb7ec4f46bfa9bbdb
Angelos Giannourakos <angelos.giannourakos@gmail.com>
2023-11-14 11:55:51 +0200

    cleaning up the code

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
 const screen = document.querySelector('#screen');
-const btns = document.querySelector('.items');
+let btns = document.querySelector('.items');
 
 let result;
-
-
-
-btns.addEventListener('click', function returnValues(e) {
-        const {target} = e;
-        const {value} = target;
-
-        if(!target.matches('button')){
-            return;
-        } else {
-
-        }
-
-        // btns.forEach((item, index )=> {
-        // console.log(Number(item.value));
-        //     // return screen.innerText = Number(item[index-1].value);
-        // });    
-     
-});
 
