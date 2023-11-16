75a1dd379f98e5eb25dcedd184994ef48a4509d9
Angelos Giannourakos <angelos.giannourakos@gmail.com>
2023-11-16 08:46:50 +0200

    Adding functionality
    need work

--- main.js
+++ main.js
@@ -1,13 +1,64 @@
-const screen = document.querySelector('#screen');
-// let btn7 = document.querySelector('.btn-7');
 let currentInput = '';
 let operator = '';
+let firstOperand = '';
 let result ='';
+const screen = document.querySelector('#screen');
+let buttons = document.querySelectorAll('.item-container button');
+console.log(buttons);
 
-btn7.addEventListener('click', () => {console.log(btn7.value)});
+// btn7.addEventListener('click', () => {console.log(btn7.value)});
+
 
 
 function appendInput(number) {
     currentInput += number;
-    screen.innerHTML = currentInput;
+    screen.value = currentInput;
 }
+
+function appendOperator(oper) {
+    if(currentInput !== '') {
+        firstOperand = currentInput;
+        currentInput = '';
+        operator = oper;
+    }
+}
+
+// Calculating the result
+function calcResult() {
+    if(currentInput!== '' && operator!== '') {
+        const secondOperand = currentInput;
+        result = eval(`${firstOperand}  ${operator} ${secondOperand}`);
+        currentInput = result;
+        screen.value = result;
+        operator = '';
+    }
+}
+
+// Clearing the screen
+function clearScreen() {
+    currentInput = '';
+    operator = '';
+    firstOperand = '';
+    result = '';
+    screen.value = '';
+}
+
+buttons.forEach( (button) => {
+    button.addEventListener('click', () => {
+        console.log(button.value);
+        const value = button.textContent;
+        if(!isNaN(value)){
+
+           appendInput(value);
+        }
+        else if(value === '+' || value === '-' || value === '*' || value === '/') {
+                    console.log(value)
+            appendOperator(value);
+        } else if(value === '=') {
+                    console.log(value)
+            calcResult();
+        } else if(value === 'C') {
+            clearScreen();
+        }
+    })
+})
