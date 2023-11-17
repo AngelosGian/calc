25929c55b9ccec68c7e553585e6e4d49ebdcd2d2
Angelos Giannourakos <angelos.giannourakos@gmail.com>
2023-11-17 18:08:20 +0200

    adding functionality of the calculator

--- main.js
+++ main.js
@@ -3,7 +3,12 @@
 // I want to show the result of the calculation
 
 // How to target the buttons:
+let currentInput = '';
+let firstOperand = '';
+let currentOperator = '';
 let display = document.querySelector('#screen');
+// display.value = '0';
+// let displayVal = display.value;
 const keys = document.querySelectorAll('.btn');
 
 keys.forEach( key => key.addEventListener('click', (event) => {
@@ -11,14 +16,60 @@ keys.forEach( key => key.addEventListener('click', (event) => {
     const btn = target.value;
     console.log(btn);
 
-    clearScreen(btn);
-    if(!isNaN(btn)) {
-        display.value = Number(btn);
+
+
+    if(!isNaN(btn) || btn === '.') {
+        appendNumber(btn);
+        // display.value = btn;
+    } else if(btn === '+' || btn === '-' || btn === '*' || btn === '/'){
+        appendOperator(btn);   
+    } else if(btn === '='){
+        calculateResults();
+    } else if(btn === 'C') {
+        clearScreen(btn);   
     }
 }))
 
-function clearScreen(clearBtn) {
-    if(clearBtn === 'C') {
-        display.value = 0;
+function appendNumber(values){
+    currentInput += values;
+    display.value = currentInput;
+}
+
+function appendOperator(operator){
+    if(currentInput !== ''){
+        firstOperand = currentInput;
+        currentInput = '';
+        currentOperator = operator;
     }
 }
+
+
+// Calculate there results
+function calculateResults(){
+    if(firstOperand !== '' && currentInput !== ''){
+        const secondOperand = currentInput;
+        const result = eval(`${firstOperand} ${currentOperator} ${secondOperand}`);
+        display.value = result;
+        currentInput = result;
+        currentOperator = '';
+ 
+    }
+}
+// Clear screen values
+// function clearScreen(clearBtn) {
+//     if(clearBtn === 'C') {
+//         display.value = 0;
+//     }
+//}
+function clearScreen(clearBtn) {
+  currentInput = '';
+  currentOperator = '';
+  firstOperand = '';
+  display.value = '';
+}
+
+
+
+// function calc(val){
+    
+// }
